d3085c5b156eb40d567fc25ec01907a22d5dbdac
Kim DaeSeong <kimds5344@naver.com>
2020-10-27 16:19:46 +0900

    This is Coding Test with Python

--- p178.py
+++ p178.py
@@ -1,13 +1,13 @@
-n = int(input())
-number = []
+n = int(input()) #숫자 입력
+number = [] #배열
 
 for i in range(0, n):
-    k = int(input())
+    k = int(input()) #숫자로 변환 후 배열에 넣어주기
     number.append(k)
 
-for i in range(0, n - 1):
+for i in range(0, n - 1): #버블 정렬
     for j in range(0, n - 1):
-        if number[j] < number[j + 1]:
+        if number[j] < number[j + 1]: #큰 수 순으로
             number[j], number[j + 1] = number[j + 1], number[j]
 
 for i in range(0, n):
