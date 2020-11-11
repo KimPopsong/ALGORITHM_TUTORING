38b39e7e21181c6dcef3f25aaee972adb36a0273
Kim DaeSeong <kimds5344@naver.com>
2020-11-11 21:43:21 +0900

    2020. 11. 11.
    시간복잡도가 O(logN)보다 작아야함

--- p368.py
+++ p368.py
@@ -6,9 +6,20 @@ s = [int(i) for i in s]
 
 fixedPoint = -1
 
-for i in range(0, n):
-    if i == s[i]:
-        fixedPoint = i
+left = 0
+right = n - 1
+
+while right >= left:
+    mid = (left + right) // 2
+
+    if mid == s[mid]:
+        fixedPoint = mid
         break
+
+    elif mid < s[mid]:
+        right = mid - 1
+
+    else:
+        left = mid + 1
 
 print(fixedPoint)
